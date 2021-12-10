f1ca86d742c007f0a5d016f71526f53dd784867f
Arash Mehdipour <arash201221@gmail.com>
2021-12-10 15:37:00 +0330

    Add DbContext

---
+++ Persistence/Context/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Users;
+using Infrastructure.Interfaces.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Persistence.Context
+{
+    public class DatabaseContext : DbContext, IDatabaseContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
