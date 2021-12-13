15e85378872c7713e09d367856c8855e9733260f
Arash Mehdipour <arash201221@gmail.com>
2021-12-13 20:19:33 +0330

    Login && Register && Logout

--- Website.Endpoint/Controllers/AccountController.cs
+++ Website.Endpoint/Controllers/AccountController.cs
@@ -1,16 +1,102 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Website.Endpoint.Models.User.ViewModels;
 
 namespace Website.Endpoint.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Login()
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+
+        public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            User newUser = new User()
+            {
+                Email = model.Email,
+                UserName = model.Email,
+                FullName = model.FullName,
+                PhoneNumber = model.PhoneNumber,
+            };
+
+            var result = await _userManager.CreateAsync(newUser, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Profile));
+            }
+
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError(item.Code, item.Description);
+            }
+            return View(model);
+        }
+
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return View(new LoginViewModel() { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.FindByNameAsync(model.Email);
+            if (user == null)
+            {
+                ModelState.AddModelError("", "کاربری با این مشخصات یافت نشد");
+                return View(model);
+            }
+            await _signInManager.SignOutAsync();
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, true);
+            if (result.Succeeded)
+            {
+                return LocalRedirect(model.ReturnUrl);
+            }
+            if (result.RequiresTwoFactor)
+            {
+                // RequiresTwoFactor Code
+            }
+
+            return View(model);
+        }
+
+        public IActionResult LogOut()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction("Index","Home");
         }
     }
 }
---
+++ Website.Endpoint/Models/User/ViewModels/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website.Endpoint.Models.User.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "لطفا ایمیل خود را وارد نمایید")]
+        [Display(Name = "ایمیل")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "لطفا کلمه عبور خود را وارد نمایید")]
+        [Display(Name = "کلمه عبور")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
